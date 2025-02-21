5c95af1dae4b968624547ed81f97d9d7fd4669bd
Roman Berger <romanberger001@gmail.com>
2025-02-21 08:58:50 +0000

    babies first internal action

---
+++ src/main/java/myLib/knowledgeBase.java
@@ -0,0 +1,9 @@
+package myLib;
+
+import jason.asSemantics.*;
+import jason.asSyntax.*;
+
+public class knowledgeBase extends DefaultInternalAction {
+	public Object execute(TransitionSystem ts, Unifier un, Term[] args)
+			 throws Exception {};
+}
